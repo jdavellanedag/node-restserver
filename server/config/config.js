4cb358208feed8c57edcac09ccce443cbe6cdee4
j.avellaneda.galindo <j.avellaneda.galindo@accenture.com>
2020-04-08 10:53:10 -0500

    Rest server - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,5 +1,21 @@
 // ============
 // Puerto
 // ============
-
 process.env.PORT = process.env.PORT || 3000
+
+// ============
+// Entorno
+// ============
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
+
+// ============
+// DB
+// ============
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://jdavellaneda:avW7vaQgbcQvihEl@cluster0-bsf4n.mongodb.net/Cafe';
+}
+
+process.env.URLDB = urlDB;
